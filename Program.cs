e5f561fc0077c2535708b4a1487ae9e2831ba484
Artem Artemev <artem@artemev.su>
2023-01-30 20:39:41 +0300

    task3done

--- Program.cs
+++ Program.cs
@@ -14,15 +14,35 @@
 
 //Task2
 
-int SumFromMtoN(int m, int n)
+// int SumFromMtoN(int m, int n)
+// {
+//     if (m > n)
+//         return 0;
+//     return m + SumFromMtoN(m + 1, n);
+// }
+// Console.Write("Введите число M: ");
+// int m = int.Parse(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int n = int.Parse(Console.ReadLine());
+// int sum = SumFromMtoN(m, n);
+// Console.WriteLine("Сумма чисел между М и N = " + sum);
+
+//Task3
+
+int Ackermann(int m, int n)
 {
-    if (m > n)
-        return 0;
-    return m + SumFromMtoN(m + 1, n);
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return Ackermann(m - 1, 1);
+    else if (m > 0 && n > 0)
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    else
+        return -1;
 }
 Console.Write("Введите число M: ");
 int m = int.Parse(Console.ReadLine());
 Console.Write("Введите число N: ");
 int n = int.Parse(Console.ReadLine());
-int sum = SumFromMtoN(m, n);
-Console.WriteLine("Сумма чисел между М и N = " + sum);
+int result = Ackermann(m, n);
+Console.WriteLine("Функция Аккермана = " + result);
