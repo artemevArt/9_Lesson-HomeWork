77ceb91b06658c58a33de6cc71433cb1d1cc8c8d
Artem Artemev <artem@artemev.su>
2023-01-30 20:38:11 +0300

    task2done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 ﻿//Task1
 
-void printNumbers(int n)
-{
-    if (n == 0)
-        return;
-    Console.Write(n + " ");
-    printNumbers(n - 1);
-}
+// void printNumbers(int n)
+// {
+//     if (n == 0)
+//         return;
+//     Console.Write(n + " ");
+//     printNumbers(n - 1);
+// }
 
-Console.Write("Введите число: ");
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// printNumbers(n);
+
+//Task2
+
+int SumFromMtoN(int m, int n)
+{
+    if (m > n)
+        return 0;
+    return m + SumFromMtoN(m + 1, n);
+}
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
 int n = int.Parse(Console.ReadLine());
-printNumbers(n);
+int sum = SumFromMtoN(m, n);
+Console.WriteLine("Сумма чисел между М и N = " + sum);
