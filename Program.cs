0877899323a39d223f2e43c94404d7d7a4d0c51c
Artem Artemev <artem@artemev.su>
2023-01-30 20:33:57 +0300

    task1done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//Task1
+
+void printNumbers(int n)
+{
+    if (n == 0)
+        return;
+    Console.Write(n + " ");
+    printNumbers(n - 1);
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+printNumbers(n);
